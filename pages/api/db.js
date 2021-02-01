7dba6ec825bcd956dc1bc869b2ebf0acb2ba6e31
Nikolas <nikolaspg@hotmail.com>
2021-02-01 18:53:44 -0300

    cancel api

--- pages/api/db.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line import/no-unresolved
-import db from '../../../db.json';
-
-export default function (req, res) {
-  res.json(db);
-}
